dccd8f10a5a989eeab48ea53622881938c26d482
Muneth <muneetchaudhary@me.com>
2021-01-05 13:42:11 +0100

    aaa

--- config.php
+++ config.php
@@ -1,14 +1,23 @@
 <?php
 ob_start();
 
-try {
+// try {
 
-    $con = new PDO("mysql:dbname=doodle;host=localhost", "root", "");
-    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-}
-catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+//     $con = new PDO("mysql:dbname=doodle;host=localhost", "root", "");
+//     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+// }
+// catch(PDOException $e) {
+//     echo "Connection failed: " . $e->getMessage();
 
-}
+// }
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
 
 ?>
