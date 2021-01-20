bc8301ea3ab42f7695641fa6d35e88835b07e9b3
Muneth <muneetchaudhary@me.com>
2021-01-20 10:22:06 +0100

    db fix

--- config.php
+++ config.php
@@ -1,23 +1,23 @@
 <?php
-ob_start();
+// ob_start();
 
-try {
+// try {
 
-    $con = new PDO("mysql:dbname=doodle;host=localhost", "root", "");
-    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-}
-catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+//     $con = new PDO("mysql:dbname=doodle;host=localhost", "root", "");
+//     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+// }
+// catch(PDOException $e) {
+//     echo "Connection failed: " . $e->getMessage();
 
-}
+// }
 
-// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-// $server = $url["host"];
-// $username = $url["user"];
-// $password = $url["pass"];
-// $db = substr($url["path"], 1);
+$server = $url["eu-cdbr-west-03.cleardb.net"];
+$username = $url["b26eaa157f67ab"];
+$password = $url["af8f126a"];
+$db = substr($url["path"], 1);
 
-// $conn = new mysqli($server, $username, $password, $db);
+$conn = new mysqli($server, $username, $password, $db);
 
 ?>
